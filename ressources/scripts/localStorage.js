3a7f7701555de06b90eba2aefa7e444d70a58155
ArneLeon <96587651+ArneLeon@users.noreply.github.com>
2025-05-01 21:08:16 +0200

    Final Design

--- ressources/scripts/localStorage.js
+++ ressources/scripts/localStorage.js
@@ -1,32 +1,18 @@
 let lightmode = localStorage.getItem('lightmode');
-let background = document.querySelector('.background');
-let sun = document.querySelector('.sun');
-let moon = document.querySelector('.moon');
-let earth = document.querySelector('.earth');
-let landscape = document.querySelector('.landscape');
+let skills = localStorage.getItem('skills');
+
+if (lightmode === 'active') {
+    enableLightmode();
+};
 
 const enableLightmode = () => {
     document.body.classList.add('lightmode');
     localStorage.setItem('lightmode', 'active');
-    moon.setAttribute('id', 'spin');
-    sun.setAttribute('id', 'spin');
-    background.setAttribute('id', 'lightmode');
-    earth.setAttribute('id', 'earthDay');
-    landscape.setAttribute('id', 'landscapeDay');
 };
 
 const disableLightmode = () => {
     document.body.classList.remove('lightmode');
     localStorage.setItem('lightmode', null);
-    moon.removeAttribute('id', 'spin');
-    sun.removeAttribute('id', 'spin');
-    background.removeAttribute('id', 'lightmode');
-    earth.removeAttribute('id', 'earthDay');
-    landscape.removeAttribute('id', 'landscapeDay');
-};
-
-if (lightmode === 'active') {
-    enableLightmode();
 };
 
 function switchTheme() {
