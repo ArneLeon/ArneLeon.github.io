856dce0e368caf459e8ba4f330435a5b9895dadd
ArneLeon <96587651+ArneLeon@users.noreply.github.com>
2025-05-02 19:12:33 +0200

    English

--- ressources/scripts/localStorage.js
+++ ressources/scripts/localStorage.js
@@ -1,10 +1,6 @@
 let lightmode = localStorage.getItem('lightmode');
 let skills = localStorage.getItem('skills');
 
-if (lightmode === 'active') {
-    enableLightmode();
-};
-
 const enableLightmode = () => {
     document.body.classList.add('lightmode');
     localStorage.setItem('lightmode', 'active');
@@ -15,6 +11,10 @@ const disableLightmode = () => {
     localStorage.setItem('lightmode', null);
 };
 
+if (lightmode === 'active') {
+    enableLightmode();
+};
+
 function switchTheme() {
     lightmode = localStorage.getItem('lightmode');
     if (lightmode !== "active") {
@@ -23,3 +23,35 @@ function switchTheme() {
         disableLightmode();
     }
 };
+
+
+// Language Switch
+let language = localStorage.getItem('language');
+let germanText = document.querySelectorAll('#germanText');
+let englishText = document.querySelectorAll('#englishText');
+
+const enableEnglish = () => {
+    germanText.forEach(text => {text.style.display = 'none'});
+    englishText.forEach(text => {text.style.display = 'block'});
+    localStorage.setItem('language', 'english');
+}
+
+const disableEnglish = () => {
+    germanText.forEach(text => {text.style.display = 'block'});
+    englishText.forEach(text => {text.style.display = 'none'});
+    localStorage.setItem('language', 'german');
+}
+
+if (language === 'english') {
+    document.querySelector('#language').value = "english";
+    enableEnglish();
+}
+
+function switchLanguage() {
+    language = localStorage.getItem('language');
+    if (language !== 'english') {
+        enableEnglish();
+    } else {
+        disableEnglish();
+    }
+}
