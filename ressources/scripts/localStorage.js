c28ac9cc109a26ae3416c23c7a6b2d38e3f2d123
ArneLeon <96587651+ArneLeon@users.noreply.github.com>
2025-05-03 01:17:04 +0200

    full functionality

--- ressources/scripts/localStorage.js
+++ ressources/scripts/localStorage.js
@@ -1,5 +1,4 @@
 let lightmode = localStorage.getItem('lightmode');
-let skills = localStorage.getItem('skills');
 
 const enableLightmode = () => {
     document.body.classList.add('lightmode');
@@ -34,18 +33,18 @@ const enableEnglish = () => {
     germanText.forEach(text => {text.style.display = 'none'});
     englishText.forEach(text => {text.style.display = 'block'});
     localStorage.setItem('language', 'english');
-}
+};
 
 const disableEnglish = () => {
     germanText.forEach(text => {text.style.display = 'block'});
     englishText.forEach(text => {text.style.display = 'none'});
     localStorage.setItem('language', 'german');
-}
+};
 
 if (language === 'english') {
     document.querySelector('#language').value = "english";
     enableEnglish();
-}
+};
 
 function switchLanguage() {
     language = localStorage.getItem('language');
@@ -54,4 +53,61 @@ function switchLanguage() {
     } else {
         disableEnglish();
     }
+};
+
+
+// Skills buttons
+let skills = localStorage.getItem('skills');
+let skillsList = document.querySelectorAll('.skills input');
+let projects = document.querySelectorAll('#projects a');
+let stations = document.querySelectorAll('#aboutMe a');
+
+
+function filterSkills() {
+    let checkedList = document.querySelectorAll('.skills input:checked');
+    if (checkedList) {
+        let checkedIds = [];
+        checkedList.forEach(skill => {
+            checkedIds.push(skill.id);
+        })
+        localStorage.setItem('skills', checkedIds);
+
+
+        let showedProjects = [];
+        projects.forEach(project => {
+            if (checkedIds.every(id => project.classList.contains(id))) {
+                showedProjects.push(project);
+            }
+        })
+
+        let showedStations = [];
+        stations.forEach(station => {
+            if (checkedIds.every(id => station.classList.contains(id))) {
+                showedStations.push(station);
+            }
+        })
+
+        projects.forEach(project => {
+            project.style.display = 'none';
+        })
+        showedProjects.forEach(project => {
+            project.style.display = 'block';
+        })
+
+        stations.forEach(station => {
+            station.style.display = 'none';
+        })
+        showedStations.forEach(station => {
+            station.style.display = 'block';
+        })
+
+    } else {
+        projects.forEach(project => {
+            project.style.display = 'block';
+        })
+        stations.forEach(station => {
+            station.style.display = 'block';
+        })
+    }
 }
+
